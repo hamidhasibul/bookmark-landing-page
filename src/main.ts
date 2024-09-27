028fe22cf03847e73dbb7768e7ad065ed1acc62a
Hasibul Hamid <hamidhasibul@gmail.com>
2024-09-27 22:07:26 +0600

    init

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", onTabClick);
+});
+
+function onTabClick(e) {
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      "border-softRed",
+      "border-b-4",
+      "md:border-b-0"
+    );
+  });
+
+  panels.forEach((panel) => {
+    panel.classList.add("hidden");
+  });
+
+  e.target.classList.add("border-softRed", "border-b-4", "md:border-b-0");
+
+  const classString = e.target.getAttribute("data-target");
+
+  document
+    .getElementById("panel")
+    ?.getElementsByClassName(classString)[0]
+    .classList.remove("hidden");
+}
